085a0f90251aa740045b610021a76ed886e2eb5b
turkia <turkia@cs.helsinki.fi>
2012-09-05 22:52:13 +0300

    Some cleanup.

--- src/p2-noinf.rb
+++ src/p2-noinf.rb
@@ -1,64 +1,69 @@
 #!/usr/bin/env ruby
-# Prototype of algoritm P2 for simulation and learning.
-# End of source NOT marked with [inf,inf], unlike in the article pseudocode.
-# Copyright Mika Turkia
-# May 15th, 2003
 
-# dscale
-#s = [[0,62], [24,64], [48,66], [72, 67], [96,69],[120,71],[144,73],[168,74]]
-#p = [[0,62], [24,64]]
-
-# polytest
-s = [[0,65], [0,69], [0,72], [200,64], [200,67], [200,72], [400,62],[400,65],[600,60],[600,64],[600,72]]
-p = [[0,72], [200,72], [400,62], [600,72]]
-
-# minus infinity
-f = [-99999, -99999]
-c = 0
-
-# array used as a priority queue
-# priority queue items: [i, f[i]]
-# i.e. index to pattern and difference vector
-pq = []
-
-# array containing indexes to source
-q = []
-
-# all indexes refer to first point in the source
-p.size.times do q.push(0) end
-
-# push difference vectors between first source note and all pattern notes to priority queue
-p.size.times do |i| pq.push([i, [s[0][0] - p[i][0], s[0][1] - p[i][1]]]) end
+# we use a priority queue implementation from https://github.com/kanwei/algorithms
+require 'algorithms'
 
 
-(p.size * s.size).times do
+class MIR 
 
-	# sort the array used as priority queue
-	pq.sort! do |a,b| a[1] <=> b[1] end
+	# Prototype of algoritm P2. End of source NOT marked with [inf,inf], unlike in the article pseudocode.
+	# Copyright Mika Turkia, May 15th, 2003 and September 9th, 2012. 
+	def self.p2(s, p)
 
-	# get minimum element
-	temp = pq.shift
+		# minus infinity
+		f = [-99999, -99999]
+		c = 0
+		matches = []
 
-	# pattern index of the difference vector
-	ii = temp[0]
+		# priority queue items are of form [i, f[i]],
+		# where i is an index to pattern, and f[i] is a difference vector. 
+		# priority queue returns the item with the smallest difference vector first.
+		pq = Containers::PriorityQueue.new { |a, b| (a <=> b) == -1 }
 
-	# difference vector
-	fn = temp[1]
+		# array containing indexes to source. 
+		# initially, all indexes refer to first point in the source.
+		q = Array.new(p.size) { 0 }
 
-	# move to next note in the source
-	q[ii] += 1
+		# push difference vectors between first source note and all pattern notes to priority queue.
+		p.size.times do |i| 
+			vec = [s[0][0] - p[i][0], s[0][1] - p[i][1]]
+			pq.push([i, vec], vec)
+		end
 
-	# update the difference vector of this pattern index
-	# note: we must check the end of source
-	pq.push([ ii, [s[q[ii]][0] - p[ii][0], s[q[ii]][1] - p[ii][1]] ]) if q[ii] < s.size
+		(p.size * s.size).times do
 
-	if fn == f then c += 1
-	else 
-		# if c may be 1, also f=-infinity matches
-		if c >= 2 then puts "match: f=(#{f[0]},#{f[1]}) c=#{c}" end
-		f = fn
-		c = 1
+			# get the smallest difference vector.
+			# ii = pattern index of the difference vector
+			# fn = difference vector
+			ii, fn = pq.pop
+
+			# move to next note in the source.
+			q[ii] += 1
+
+			# update the difference vector of this pattern index.
+			# note: we must check the end of source.
+			if q[ii] < s.size then
+				vec = [s[q[ii]][0] - p[ii][0], s[q[ii]][1] - p[ii][1]]
+				pq.push([ii, vec], vec) 
+			end
+
+			if fn == f then c += 1
+			else 
+				# if c may be 1, also f=-infinity matches
+				matches << {:f => f, :c => c} if c >= 2 
+				f = fn
+				c = 1
+			end
+		end
+		matches
 	end
 end
 
+
+# testing 
+
+source  = [[0,65], [0,69], [0,72], [200,64], [200,67], [200,72], [400,62],[400,65],[600,60],[600,64],[600,72]]
+pattern = [[0,72], [200,72], [400,62], [600,72]]
+
+puts MIR::p2(source, pattern)
 
--- src/shiftorand.rb
+++ src/shiftorand.rb
@@ -1,27 +1,39 @@
 #!/usr/bin/env ruby
 
-# Prototype of ShiftOrAnd algorithm
-# No transposition invariance, exact matches only.
-# Copyright Mika Turkia 2002
+# Copyright Mika Turkia 2002/2012
+class MIR
 
-chords = [[60, 62, 64], [64, 66]]
-pattern = [62, 66]
+	# Prototype of ShiftOrAnd algorithm. 
+	# No transposition invariance, exact matches only.
+	def self.shiftorand(chords, pattern)
 
-m = nil
-t = []
+		matches = []
+		m = nil
+		t = []
+		mask = e = 2 ** pattern.size - 1
+		tmppre = 2 ** 32 - 1
 
-mask = e = 2 ** pattern.size - 1
-tmppre = 2 ** 32 - 1
+		for i in 0..127 do t[i] = mask end
 
-for i in 0..127 do t[i] = mask end
-for i in 0...pattern.size do t[pattern[i]] -= 2 ** i end
+		for i in 0...pattern.size do t[pattern[i]] -= 2 ** i end
 
-for j in 0...chords.size do
-	tmp = tmppre
-	chords[j].each do |note| tmp &= t[note] end
-	e = ((e << 1) | tmp) & mask
-	if (e[pattern.size - 1] == 0) then
-		puts "Match: " + (j-pattern.size+1).to_s + "-" + j.to_s
+		for j in 0...chords.size do
+
+			tmp = tmppre
+			chords[j].each do |note| tmp &= t[note] end
+			e = ((e << 1) | tmp) & mask
+
+			matches << {:start => j - pattern.size + 1, :end => j} if (e[pattern.size - 1] == 0)
+		end
+		matches
 	end
 end
+
+
+# testing 
+
+chords = [[60, 62, 64], [64, 66], [62, 68], [66, 68]]
+pattern = [62, 66]
+
+puts MIR::shiftorand(chords, pattern)
 
